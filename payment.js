bfbf29b9b7c5f1866a74a4826e7d7640a0a624d7
Prajval Kedlaya <73179398+prajva@users.noreply.github.com>
2026-09-07 23:20:09 +0530

    Create payment.js

---
+++ payment.js
@@ -0,0 +1,11 @@
+function calculateDiscount(price, discountPercent) {
+  // Bug: No check if discountPercent is negative or > 100
+  // Bug: price can be null or undefined
+  const finalPrice = price - (price * (discountPercent / 100));
+  return finalPrice.toFixed(2);
+}
+
+function getUserProfile(user) {
+  // Bug: user can be null or undefined; accessing user.email crashes!
+  return user.email.toLowerCase();
+}
